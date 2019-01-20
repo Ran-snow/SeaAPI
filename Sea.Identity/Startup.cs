e9d3759ab7b799f2052d07e0cb04ae3f802b9ae3
Tom <18158510+Ran-snow@users.noreply.github.com>
2019-01-20 21:38:08 +0800

    添加权限认证

--- Sea.Identity/Startup.cs
+++ Sea.Identity/Startup.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace Sea.API
+namespace Sea.Identity
 {
     public class Startup
     {
@@ -34,7 +35,12 @@ namespace Sea.API
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseHsts();
+            }
 
+            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
